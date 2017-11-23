2040105023d2e743d164a16d70861ab105811756
g1eb <mail@g1eb.com>
2017-11-23 16:21:02 +0100

    Remove semi-colons from js source files

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import '../styles/App.css';
-import logo from '../images/logo.svg';
+import '../styles/App.css'
+import logo from '../images/logo.svg'
 
 class App extends Component {
   render() {
@@ -15,8 +15,8 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import './styles/index.css';
-import App from './components/App';
+import './styles/index.css'
+import App from './components/App'
 
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
-registerServiceWorker();
+registerServiceWorker()
