8e17edf975c873b297b3c237fc70dc09c46a49fa
Silvia <silvia@stamkracht.com>
2017-11-23 16:57:19 +0100

    Add initial map component

---
+++ src/components/Map.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import '../styles/map.css'
+
+class Map extends Component {
+  render() {
+    return (
+      <div className="c-map">
+        <p className="c-map--intro">
+          this is the map component
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Map
