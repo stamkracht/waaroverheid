5e5c00f83f61a45f818584c23e3cca1d4dd98744
g1eb <mail@g1eb.com>
2017-11-23 16:19:43 +0100

    Organize  src folder for specific sub-folders for images, styles and js files

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import '../styles/App.css';
+import logo from '../images/logo.svg';
 
 class App extends Component {
   render() {
