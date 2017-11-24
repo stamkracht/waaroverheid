6b816e30d12d8caae1973591b824c8376d85e56d
Silvia <silvia@stamkracht.com>
2017-11-24 12:19:26 +0100

    Add default props for button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,15 +4,21 @@ import Container from './Container'
 import '../styles/button.css'
 
 class Button extends React.Component {
+
+
   render() {
     return (
       <div className='c-button'>
         <Container>
-          {this.props.children}
+          {this.props.text}
         </Container>
       </div>
     )
   }
+}
+
+Button.defaultProps = {
+  text: 'Click me',
 }
 
 export default Button;
