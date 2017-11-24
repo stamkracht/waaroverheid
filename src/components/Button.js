be7de0badba598ea7a6e378c5c0957ca0b43b854
Silvia <silvia@stamkracht.com>
2017-11-24 13:40:14 +0100

    Add function renderIcon

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,22 @@ import React from 'react'
 
 import Container from './Container'
 import '../styles/button.css'
+import document from '../images/document.svg'
+
 
 class Button extends React.Component {
-
+  renderIcon() {
+    if(this.props.icon) {
+      return <img src={document} className='c-button--icon' alt="document" />
+    }
+  }
 
   render() {
     return (
       <div className='c-button'>
         <Container>
-          {this.props.text}
+          <div className='c-button--text'>{this.props.text}</div>
+          {this.renderIcon()}
         </Container>
       </div>
     )
@@ -19,6 +26,7 @@ class Button extends React.Component {
 
 Button.defaultProps = {
   text: 'Click me',
+  icon: '',
 }
 
 export default Button;
