e452b296b005a91ff9bf3bdb58199d4fac7cf882
Silvia <silvia@stamkracht.com>
2017-11-27 16:43:23 +0100

    Make badge component active

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,13 +3,22 @@ import React from 'react'
 import '../styles/badge.css'
 
 class Badge extends React.Component {
+
   render() {
-    return  <div className='c-badge'>{this.props.num}</div>
+
+    let className = 'c-badge'
+
+    if ( this.props.active ) {
+      className = 'c-badge active'
+    }
+
+    return  <div className={className}>{this.props.num}</div>
   }
 }
 
 Badge.defaultProps = {
   num: 0,
+  active: false,
 }
 
 export default Badge;
