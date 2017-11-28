06ff6a8c68749583340ec26715c57cccd42aa660
g1eb <mail@g1eb.com>
2017-11-28 11:09:20 +0100

    Add leaflet to the map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
+import { Map as LeafletMap, TileLayer } from 'react-leaflet'
 
 import '../styles/map.css'
 
 class Map extends Component {
   render() {
+    let position = [52.371, 4.925]
+    let zoom = 17
     return (
       <div className="c-map">
-        <p className="c-map--intro">
-          this is the map component
-        </p>
+        <LeafletMap center={position} zoom={zoom}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
+        </LeafletMap>
       </div>
     )
   }
