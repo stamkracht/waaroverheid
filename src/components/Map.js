84830c198b986bd198573ebece077f09ccb5b1ef
g1eb <mail@g1eb.com>
2017-11-28 14:42:13 +0100

    Use https for stamen tiles url

--- src/components/Map.js
+++ src/components/Map.js
@@ -12,7 +12,7 @@ class Map extends Component {
         <LeafletMap center={position} zoom={zoom}>
           <TileLayer
             attributoin='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            url='http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png'
+            url='https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png'
           />
         </LeafletMap>
       </div>
