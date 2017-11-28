ec26176b9f0fd164e20014d044656f30e2cc7fd1
g1eb <mail@g1eb.com>
2017-11-28 11:17:05 +0100

    Change header title to WaarOverheid

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="c-app">
         <header className="c-app c-app--header">
           <img src={logo} className="c-app--logo" alt="logo" />
-          <h1 className="c-app--title">Welcome to React</h1>
+          <h1 className="c-app--title">WaarOverheid</h1>
         </header>
         <p className="c-app--intro">
           To get started, edit <code>src/App.js</code> and save to reload.
