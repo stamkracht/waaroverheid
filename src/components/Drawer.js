7e02df94e62ace4cc8d8079e7f5302ed9d399fde
Silvia <silvia@stamkracht.com>
2017-11-28 13:49:20 +0100

    Add hamburger button to drawer

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -6,21 +6,47 @@ import '../styles/button.css'
 
 class Drawer extends React.Component {
 
-  render() {
-
-    return (
-        <div className='c-drawer'>
-            <img className='c-drawer--icon' src={document} alt="document icon" />
-            <span className='c-drawer--text'>{this.props.text}</span>
-        </div>
-    )
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeHam: false
+    }
   }
 
+  handleOnClick() {
+    this.setState({activeHam: !this.state.activeHam})
+  }
+
+    // if(this.hamState === true) {
+    //   this.hamState = false
+    // else {
+    //   this.hamState = true
+    //   this.forceUpdate()
+    // }
+
+  render() {
+
+  let className = 'hamburger'
+  if(this.state.activeHam === true) {
+    className += ' is-active'
+  }
+
+  return (
+      <div className='c-drawer'>
+          <img className='c-drawer--icon' src={document} alt="document icon" />
+          <span className='c-drawer--text'>{this.props.text}</span>
+          <div className={className} onClick={this.handleOnClick.bind(this)}>
+            <span className='line'></span>
+            <span className='line'></span>
+            <span className='line'></span>
+          </div>
+      </div>
+  )
+  }
 }
 
 Drawer.defaultProps = {
   text: 'Documents',
 }
-
 
 export default Drawer
