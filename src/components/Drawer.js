9c816480ca86c0e5806fed86e2c3405a336f4189
Silvia <silvia@stamkracht.com>
2017-11-29 11:07:52 +0100

    Make drawer expandable

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -9,23 +9,27 @@ class Drawer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      activeHam: false
+      activeHam: false,
+      activeDrawer: false,
     }
   }
 
   handleOnClick() {
     this.setState({activeHam: !this.state.activeHam})
+    this.setState({activeDrawer: !this.state.activeDrawer})
   }
 
   render() {
 
+  let classDrawer = 'c-drawer'
   let className = 'hamburger'
-  if(this.state.activeHam === true) {
+  if(this.state.activeHam === true && this.state.activeDrawer === true) {
     className += ' is-active'
+    classDrawer += ' active'
   }
 
   return (
-      <div className='c-drawer' onClick={this.handleOnClick.bind(this)}>
+      <div className={classDrawer} onClick={this.handleOnClick.bind(this)}>
           <img className='c-drawer--icon' src={document} alt="document icon" />
           <span className='c-drawer--text'>{this.props.text}</span>
           <div className={className}>
