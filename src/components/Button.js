e68bc3c6d0637e543ade3f24bae652bc57fb4d3a
Silvia <silvia@stamkracht.com>
2017-11-29 16:23:38 +0100

    Add case arrow to icon prop

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,28 +5,31 @@ import '../styles/button.css'
 import document from '../images/document.svg'
 import hamburger from '../images/hamburger.svg'
 import search from '../images/search.svg'
+import arrow from '../images/arrow.svg'
 
 
 class Button extends React.Component {
 
   renderIcon() {
     if ( this.props.icon ) {
-      let className = `c-button--icon ${this.props.iconPosition}`
+      let className = `c-container--icon ${this.props.iconPosition}`
       if ( this.props.icon === 'document' ) {
-        return <img className={className} src={document} alt="document icon" />
+        return <img className={className} src={document} alt='document icon' />
       } else if ( this.props.icon === 'hamburger' ) {
-        return <img className={className} src={hamburger} alt="hamburger icon" />
+          return <img className={className} src={hamburger} alt='hamburger icon' />
+      }  else if ( this.props.icon === 'arrow' ) {
+          return <img className={className} src={arrow} alt='arrow icon' />
       } else {
-        return <img className={className} src={search} alt="search icon" />
+          return <img className={className} src={search} alt='search icon' />
+        }
       }
     }
-  }
 
   render() {
     return (
       <div className='c-button'>
         <Container>
-          <div className='c-button--text'>
+          <div className='c-container--text'>
             {this.props.text}
           </div>
           {this.renderIcon()}
