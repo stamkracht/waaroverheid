7c5645d8dbb8231665b19aaab06cb314907c40ab
Silvia <silvia@stamkracht.com>
2017-11-29 11:58:09 +0100

    Update conditional with ternary operator for active classes

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -9,36 +9,31 @@ class Drawer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      activeHam: false,
       activeDrawer: false,
     }
   }
 
   handleOnClick() {
-    this.setState({activeHam: !this.state.activeHam})
-    this.setState({activeDrawer: !this.state.activeDrawer})
+    this.setState({
+      activeDrawer: !this.state.activeDrawer,
+    })
   }
 
   render() {
 
-  let classDrawer = 'c-drawer'
-  let className = 'hamburger'
-  if(this.state.activeHam === true && this.state.activeDrawer === true) {
-    className += ' is-active'
-    classDrawer += ' active'
-  }
-
-  return (
-      <div className={classDrawer} onClick={this.handleOnClick.bind(this)}>
-          <img className='c-drawer--icon' src={document} alt="document icon" />
-          <span className='c-drawer--text'>{this.props.text}</span>
-          <div className={className}>
-            <span className='line'></span>
-            <span className='line'></span>
-            <span className='line'></span>
-          </div>
+    return (
+      <div className={`${this.state.activeDrawer ? 'c-drawer active' : 'c-drawer'}`}>
+        <div className='c-drawer--header' onClick={this.handleOnClick.bind(this)}>
+            <img className='c-drawer--icon' src={document} alt="document icon" />
+            <span className='c-drawer--text'>{this.props.text}</span>
+            <div className={`${this.state.activeDrawer ? 'c-hamburger active' : 'c-hamburger'}`}>
+              <span className='line'></span>
+              <span className='line'></span>
+              <span className='line'></span>
+            </div>
+        </div>
       </div>
-  )
+    )
   }
 }
 
