804d7e157ef13bf9123c442bdbdcae5e78f9c123
Silvia <silvia@stamkracht.com>
2017-11-30 13:01:37 +0100

    Move renderIcon and renderText from button to container

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,38 +2,18 @@ import React from 'react'
 
 import Container from './Container'
 import '../styles/button.css'
-import document from '../images/document.svg'
-import hamburger from '../images/hamburger.svg'
-import search from '../images/search.svg'
-import arrow from '../images/arrow.svg'
-
 
 class Button extends React.Component {
-
-  renderIcon() {
-    if ( this.props.icon ) {
-      let className = `c-container--icon ${this.props.iconPosition}`
-      if ( this.props.icon === 'document' ) {
-        return <img className={className} src={document} alt='document icon' />
-      } else if ( this.props.icon === 'hamburger' ) {
-          return <img className={className} src={hamburger} alt='hamburger icon' />
-      }  else if ( this.props.icon === 'arrow' ) {
-          return <img className={className} src={arrow} alt='arrow icon' />
-      } else {
-          return <img className={className} src={search} alt='search icon' />
-        }
-      }
-    }
 
   render() {
     return (
       <div className='c-button'>
-        <Container>
-          <div className='c-container--text'>
-            {this.props.text}
-          </div>
-          {this.renderIcon()}
-        </Container>
+        <Container
+          text={this.props.text}
+          textAlign={this.props.textAlign}
+          icon={this.props.icon}
+          iconPosition={this.props.iconPosition}
+          />
       </div>
     )
   }
@@ -43,6 +23,7 @@ Button.defaultProps = {
   text: 'Click me',
   icon: '',
   iconPosition: 'right',
+  textAlign: 'center',
 }
 
 export default Button;
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,16 +1,52 @@
 import React from 'react'
 
 import '../styles/container.css'
+import document from '../images/document.svg'
+import hamburger from '../images/hamburger.svg'
+import search from '../images/search.svg'
+import arrow from '../images/arrow.svg'
 
 class Container extends React.Component {
-  
+
+  renderIcon() {
+    if ( this.props.icon ) {
+      let classIcon = `c-container--icon ${this.props.iconPosition}`
+      if ( this.props.icon === 'document' ) {
+        return <img className={classIcon} src={document} alt='document icon' />
+      } else if ( this.props.icon === 'hamburger' ) {
+          return <img className={classIcon} src={hamburger} alt='hamburger icon' />
+      }  else if ( this.props.icon === 'arrow' ) {
+          return <img className={classIcon} src={arrow} alt='arrow icon' />
+      } else {
+          return <img className={classIcon} src={search} alt='search icon' />
+      }
+    }
+  }
+
+  renderText() {
+    if(this.props.text) {
+      let classText = `c-container--text ${this.props.textAlign}`
+      return <div className={classText}> {this.props.text} </div>
+    }
+  }
+
   render() {
     return (
       <div className='c-container'>
         {this.props.children}
+        {this.renderText()}
+        {this.renderIcon()}
       </div>
     )
   }
 }
+
+Container.defaultProps = {
+  text: 'Default',
+  icon: '',
+  iconPosition: 'right',
+  textAlign: 'center',
+}
+
 
 export default Container
