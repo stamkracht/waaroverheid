434b49eb325c8c06f4bd546334c41abf73bc1491
Silvia <silvia@stamkracht.com>
2017-12-06 12:53:52 +0100

    Add submit function to searchbox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -8,16 +8,22 @@ class SearchBox extends React.Component {
 
   handleKeyPress = (event) => {
     if(event.charCode === 13) {
-      console.log('Premuto invio')
+      this.props.onSubmit(this.refs.query.value)
     }
+  }
+
+  handleClick = () => {
+   this.props.onSubmit(this.refs.query.value)
   }
 
   render() {
     return (
       <Container shadow={this.props.shadow}>
         <div className='c-searchBox'>
-          <input type='text' name='name' placeholder='Search' onKeyPress={this.handleKeyPress}/>
-          <Icon icon={this.props.icon} iconPosition={this.props.iconPosition} />
+          <input type='text' ref='query' name='name' placeholder='Search' onKeyPress={this.handleKeyPress} />
+          <div onClick={this.handleClick}>
+            <Icon icon={this.props.icon} iconPosition={this.props.iconPosition} />
+          </div>
         </div>
       </Container>
     )
