1f6ae0acc162f504b99e834c7ab89ed0628cf1fc
Silvia <silvia@stamkracht.com>
2017-12-06 11:26:03 +0100

    Add handleKeyPress function to input form in searchbox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -6,11 +6,17 @@ import '../styles/searchBox.css'
 
 class SearchBox extends React.Component {
 
+  handleKeyPress = (event) => {
+    if(event.charCode === 13) {
+      console.log('Premuto invio')
+    }
+  }
+
   render() {
     return (
       <Container shadow={this.props.shadow}>
         <div className='c-searchBox'>
-          <input type='text' name='name' placeholder='Search' />
+          <input type='text' name='name' placeholder='Search' onKeyPress={this.handleKeyPress}/>
           <Icon icon={this.props.icon} iconPosition={this.props.iconPosition} />
         </div>
       </Container>
@@ -23,6 +29,7 @@ SearchBox.defaultProps = {
   textAlign: 'left',
   icon: 'search',
   iconPosition: 'right',
+  onSubmit: undefined,
 }
 
 export default SearchBox
