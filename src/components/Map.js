fe00a684becca35fa5e1a52a0941da54ad786557
g1eb <mail@g1eb.com>
2017-12-11 18:10:04 +0100

    Hide default zoom controls

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,7 +9,7 @@ class Map extends Component {
     let zoom = 17
     return (
       <div className="c-map">
-        <LeafletMap center={position} zoom={zoom}>
+        <LeafletMap center={position} zoom={zoom} zoomControl={false}>
           <TileLayer
             attributoin='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             url='https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png'
