48b19c184540be32d37ef37055cd73c1f30c2b31
Silvia <silvia@stamkracht.com>
2017-12-12 17:07:16 +0100

    Update initial documents component

--- src/components/Documents.js
+++ src/components/Documents.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react'
 
+import Container from './Container'
+import Icon from './Icon'
 import '../styles/documents.css'
 
 class Documents extends Component {
   render() {
     return (
-      <div className="c-documents">
-        <p className="c-documents--intro">
-          this is the documents component
-        </p>
-      </div>
+      <Container shadow={true}>
+        <div className='c-documents'>
+          <div>
+            <Icon icon='file' iconPosition='left' width='50' height='50'/>
+            <div className='c-documents--text'>
+              <h1>Hey</h1>
+              <h2>Hey there</h2>
+            </div>
+          </div>
+        </div>
+      </Container>
     )
   }
 }
