9fa1d265e0af9ba076c8dbab9d450338c874c06f
g1eb <mail@g1eb.com>
2017-12-12 12:06:00 +0100

    Change the url to get utrect city neighborhoods

--- src/components/Map.js
+++ src/components/Map.js
@@ -14,7 +14,7 @@ class Map extends Component {
   }
 
   componentDidMount() {
-    fetch('http://waaroverheid.cleverdon.hum.uva.nl/municipal/0344', {
+    fetch('http://waaroverheid.cleverdon.hum.uva.nl/municipal/06/neighborhoods', {
       method: 'GET',
     })
       .then(d => d.json())
