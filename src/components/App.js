3f0826557d144d352505eef2808de38bc13773e6
g1eb <mail@g1eb.com>
2017-12-15 12:03:33 +0100

    Removed placeholder stuff from the src dir

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
 import '../styles/app.css'
-import logo from '../images/logo.svg'
 import Filters from './Filters'
 import Drawer from './Drawer'
 
@@ -17,13 +16,6 @@ class App extends Component {
   render() {
     return (
       <div className="c-app">
-        <header className="c-app c-app--header">
-          <img src={logo} className="c-app--logo" alt="logo" />
-          <h1 className="c-app--title">Welcome to React</h1>
-        </header>
-        <p className="c-app--intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Filters onSearch={this.onSearch} />
         <Drawer />
       </div>
