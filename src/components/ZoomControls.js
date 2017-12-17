7364c946532ea9ccfd5de44add788db5fb0ec2da
g1eb <mail@g1eb.com>
2017-12-17 17:01:01 +0100

    Handle click events to update zoom level of the map

--- src/components/ZoomControls.js
+++ src/components/ZoomControls.js
@@ -5,13 +5,26 @@ import Button from './Button'
 import '../styles/zoomControls.css'
 
 class ZoomControls extends React.Component {
+
+  handleOnClick(level) {
+    if ( level === 'provincie' ) {
+      this.props.setZoomLevel(9)
+    } else if ( level === 'gemeente' ) {
+      this.props.setZoomLevel(12)
+    } else if ( level === 'wijk' ) {
+      this.props.setZoomLevel(15)
+    } else if ( level === 'buurt' ) {
+      this.props.setZoomLevel(18)
+    }
+  }
+
   render() {
     return (
       <div className='c-zoomControls'>
-        <Button text='Provincie' />
-        <Button text='Gemeente' />
-        <Button text='Wijk' />
-        <Button text='Buurt' />
+        <Button text='Provincie' onClick={() => this.handleOnClick('provincie')} />
+        <Button text='Gemeente' onClick={() => this.handleOnClick('gemeente')} />
+        <Button text='Wijk' onClick={() => this.handleOnClick('wijk')} />
+        <Button text='Buurt' onClick={() => this.handleOnClick('buurt')} />
       </div>
     )
   }
