d40baf227517e1da6dabaee254657a642dd6106e
g1eb <mail@g1eb.com>
2018-01-10 16:29:51 +0100

    Init filters in the document service for dev purposes

--- src/services/DocumentService.js
+++ src/services/DocumentService.js
@@ -1,11 +1,68 @@
 class DocumentService {
 
   constructor() {
-    this.filters = {}
+    this.filters = {
+      search: '',
+      range: {
+        min: 2,
+        max: 10,
+      },
+      types: [],
+    }
+
+    this.getTypes()
   }
 
   setFilters(filters) {
     this.filters = filters
+  }
+
+  getTypes() {
+    this.filters.types.push({
+      name: 'Soorten',
+      items: [{
+        name: 'Type 1',
+        active: true,
+      }, {
+        name: 'Type 2',
+        active: false,
+      }, {
+        name: 'Type 3',
+        active: false,
+      }],
+    })
+    this.filters.types.push({
+      name: 'Parties',
+      items: [{
+        name: 'Party 1',
+        active: true,
+      }, {
+        name: 'Party 2',
+        active: true,
+      }, {
+        name: 'Party 3',
+        active: false,
+      }],
+    })
+    this.filters.types.push({
+      name: 'Spices',
+      items: [{
+        name: 'Baby',
+        active: false,
+      }, {
+        name: 'Ginger',
+        active: false,
+      }, {
+        name: 'Posh',
+        active: false,
+      }, {
+        name: 'Scary',
+        active: false,
+      }, {
+        name: 'Sporty',
+        active: false,
+      }],
+    })
   }
 }
 
