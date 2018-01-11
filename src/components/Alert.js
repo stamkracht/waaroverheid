27dfe2dcb4403dba7ee217419e163e35a2a972f9
g1eb <mail@g1eb.com>
2018-01-11 18:24:17 +0100

    Update alert component with codestyle changes and cleanup

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import 'react-responsive-modal/lib/react-responsive-modal.css'
+import Modal from 'react-responsive-modal/lib/css'
 
 import Button from './Button'
-import 'react-responsive-modal/lib/react-responsive-modal.css';
-import Modal from 'react-responsive-modal/lib/css'
-import '../styles/alert.css'
 import { validate } from '../utilities/email.js'
+
+import '../styles/alert.css'
+
 
 class Alert extends React.Component {
 
@@ -12,49 +14,85 @@ class Alert extends React.Component {
     super(props)
 
     this.state = {
-      open: false,
+      active: false,
       result: '',
     }
   }
 
-  onOpenModal = () => {
-      this.setState({ open: true })
-    };
+  onOpenModal() {
+    this.setState({active: true})
+  }
 
-  onCloseModal = () => {
-    this.setState({ open: false })
+  onCloseModal() {
+    this.setState({active: false})
     this.setState({result: ''})
-    };
+  }
 
-  handleClick = () => {
-    if(validate(this.refs.email.value) === true) {
+  handleClick() {
+    if ( validate(this.refs.email.value) )  {
       this.setState({result: 'valid'})
-    }
-    else {
+    } else {
       this.setState({result: 'invalid'})
     }
-    }
+  }
+
+  renderModalButton() {
+    return (
+      <Button
+        text='Sign up for alerts'
+        icon='mail'
+        textAlign='left'
+        onClick={this.onOpenModal.bind(this)} />
+    )
+  }
+
+  renderModalHeader() {
+    return (
+      <h2 className='c-alert--header'>
+        Sign up for alerts
+      </h2>
+    )
+  }
+
+  renderModalContent() {
+    return (
+      <div className='c-alert--content'>
+        <h3>Receive alerts for documents corresponding to these filters:</h3>
+        <input placeholder='Type in your email address' ref='email' />
+        {this.state.result === 'invalid' && <p>Please insert a valid email address!</p>}
+      </div>
+    )
+  }
+
+  renderModalFooter() {
+    return (
+      <div className='c-alert--footer'>
+        <Button
+          text='Submit'
+          shadow={true}
+          hovering={true}
+          textAlign='center'
+          onClick={this.handleClick.bind(this)} />
+      </div>
+    )
+  }
+
+  renderModal() {
+    return (
+      <Modal little open={this.state.active}
+          onClose={this.onCloseModal.bind(this)}>
+        {this.renderModalHeader()}
+        {this.renderModalContent()}
+        {this.renderModalFooter()}
+      </Modal>
+    )
+  }
 
   render() {
     return (
       <div className='c-alert'>
-        <Button text='Sign up for alerts' icon='mail' textAlign='left' onClick={this.onOpenModal}/>
-        <Modal open={this.state.open} onClose={this.onCloseModal} little>
-          <h2 className='c-alert--header'>Sign up for alerts</h2>
-          <div className='c-alert--content'>
-            <h3>Receive alerts for documents corresponding to these filters:</h3>
-            <input placeholder='Type in your email address' ref='email' />
-            {this.state.result === 'invalid' && <p>Please insert a valid email address!</p>}
-          </div>
-          <div className='c-alert--footer'>
-            <Button
-              text='Submit'
-              shadow={true}
-              hovering={true}
-              textAlign='center'
-              onClick={this.handleClick} />
-          </div>
-        </Modal>
+        {this.renderModalButton()}
+        {this.renderModal()}
       </div>
     )
   }
