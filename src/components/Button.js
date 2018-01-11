6887ff07adedbd3c4c161efc47418702a1c05400
g1eb <mail@g1eb.com>
2018-01-11 11:20:15 +0100

    Codestyle, clean-up

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,26 @@ import React from 'react'
 
 import Container from './Container'
 import Icon from './Icon'
+
 import '../styles/button.css'
+
 
 class Button extends React.Component {
 
+  getClassName() {
+    if ( this.props.hovering && this.props.disabled ) {
+      return 'c-button hovering disabled'
+    } else if ( this.props.hovering && !this.props.disabled ) {
+      return 'c-button hovering'
+    } else if ( !this.props.hovering && this.props.disabled ) {
+      return 'c-button disabled'
+    } else {
+      return 'c-button'
+    }
+  }
+
   renderText() {
-    if(this.props.text) {
+    if ( this.props.text ) {
       return (
         <div className={`c-button--text ${this.props.textAlign}`}>
           {this.props.text}
@@ -16,29 +30,21 @@ class Button extends React.Component {
     }
   }
 
-  getClassName() {
-    if(this.props.hovering && this.props.disabled) {
-        return 'c-button hovering disabled'
-      }
-    else if(this.props.hovering && !this.props.disabled) {
-        return 'c-button hovering'
-      }
-    else if(!this.props.hovering && this.props.disabled) {
-        return 'c-button disabled'
-      }
-    else {
-        return 'c-button'
-      }
-    }
+  renderIcon() {
+    return (
+      <Icon
+        icon={this.props.icon}
+        iconPosition={this.props.iconPosition}
+        pointRight={this.props.pointRight} />
+    )
+  }
 
   render() {
     return (
       <Container shadow={this.props.shadow}>
         <div className={this.getClassName()} onClick={this.props.onClick}>
-
           {this.renderText()}
-
-          <Icon icon={this.props.icon} iconPosition={this.props.iconPosition} pointRight={this.props.pointRight}/>
+          {this.renderIcon()}
         </div>
       </Container>
     )
