0649b7d90379e2095d312b279bb48df05bde7c00
SilviaPag95 <silviapagliaro95@gmail.com>
2018-01-26 14:54:59 +0100

    Modify YAxis label offset

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -31,7 +31,7 @@ class Chart extends React.Component {
             <Label value="Date of publication" offset={5} height={50} position="insideBottom" />
           </XAxis>
           <YAxis dataKey='doc_count' name='Number of Documents'>
-            <Label value='Documents' offset={15} angle={-90} position='insideLeft' style={{textAnchor: 'middle'}} />
+            <Label value='Documents' offset={12} angle={-90} position='insideLeft' style={{textAnchor: 'middle'}} />
           </YAxis>
           <CartesianGrid strokeDasharray="3 3"/>
           <Tooltip labelFormatter = {(time) => moment(time).format(`MMM 'YY`)}/>
