3d04af8b96a415fba6f58f1c70399e37bfc2ac73
SilviaPag95 <silviapagliaro95@gmail.com>
2018-02-13 11:27:48 +0100

    Final setup of resetFilters function

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,55 +1,58 @@
-import React from 'react'
+import React from 'react';
 
-import Checkbox from './Checkbox'
-import Badge from './Badge'
-import '../styles/typesList.css'
+import Checkbox from './Checkbox';
+import Badge from './Badge';
+import '../styles/typesList.css';
 
 class ListItem extends React.Component {
+    constructor(props) {
+        super(props);
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      checked: this.props.checked,
+        this.state = {
+            checked: this.props.checked
+        };
     }
-  }
 
-  handleChange = () => {
-    this.setState({
-      checked: !this.state.checked,
-    }, () => this.props.onChange(this.state.checked))
-  }
-
-  renderBadge() {
-    if ( !!this.props.badge ) {
-      return (
-        <Badge
-          num={this.props.badge}
-          active={this.state.checked} />
-      )
+    componentWillReceiveProps({ checked }) {
+        this.setState({ checked });
     }
-  }
 
-  render() {
-    return (
-      <li>
-        <Checkbox
-          id={this.props.id}
-          label={this.props.label}
-          active={this.state.checked}
-          onChange={this.handleChange} />
-          {this.renderBadge()}
-      </li>
-    )
-  }
+    handleChange = () => {
+        this.setState(
+            {
+                checked: !this.state.checked
+            },
+            () => this.props.onChange(this.state.checked)
+        );
+    };
+
+    renderBadge() {
+        if (!!this.props.badge) {
+            return <Badge num={this.props.badge} active={this.state.checked} />;
+        }
+    }
+
+    render() {
+        return (
+            <li>
+                <Checkbox
+                    id={this.props.id}
+                    label={this.props.label}
+                    active={this.state.checked}
+                    onChange={this.handleChange}
+                />
+                {this.renderBadge()}
+            </li>
+        );
+    }
 }
 
 ListItem.defaultProps = {
-  checked: false,
-  id: Math.round(Math.random()*1000),
-  label: 'this is a label',
-  onChange: undefined,
-  badge: undefined,
-}
+    checked: false,
+    id: Math.round(Math.random() * 1000),
+    label: 'this is a label',
+    onChange: undefined,
+    badge: undefined
+};
 
-export default ListItem
+export default ListItem;
